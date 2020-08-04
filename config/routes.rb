fb466acd60a30caaa5d95149f5bec277002a6a87
Balan Alina <alinabalan16.ab@gmail.com>
2020-08-04 12:47:58 +0300

    add links to nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'caffes#home'
+  get '/home' ,to: 'caffes#home'
+#  get '/about' ,to: '#'
+#  get '/service', to: '#'
+#  get '/blog', to: '#'
+#  get '/contacts', to: '#'
 end
