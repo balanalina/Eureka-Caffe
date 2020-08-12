2164cd4827bbcc3b3567e59ac92a8473d7627e23
Balan Alina <alinabalan16.ab@gmail.com>
2020-08-12 19:06:15 +0300

    done remember-me

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,32 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   has_secure_password
   before_save { email.downcase! }
   validates :name, presence: true, length: { minimum: 6 }
   validates :password_digest, presence: true, length: { minimum: 6 }
   validates :email, presence: true, uniqueness: true,
                     length: { minimum: 10 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+               BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil )
+  end
+  
+  def authenticated?(remember_token)
+    BCrypt::Password::new(remember_digest).is_password?(remember_token)
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
   config.assets.quiet = true
 
   # Use Cloudinary.
-  config.active_storage.service = :cloudinary
+  config.active_storage.service = :local
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
