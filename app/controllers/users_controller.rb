4894227b0ca324e97a6d0f81062303cddec6faff
Balan Alina <alinabalan16.ab@gmail.com>
2020-08-17 18:07:20 +0300

    authorization done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, :correct_user, only: %i[edit update show]
   def new
     @user = User.new
   end
@@ -10,11 +11,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      forwarding_url = session[:forwarding_url]
       reset_session
       log_in @user
-      flash[:success] = "Account created!"
+      flash[:success] = 'Account created!'
       Cart.create(user_id: @user.id)
-      redirect_to @user
+      redirect_to forwarding_url || @user
     else
       render 'new'
     end
@@ -38,5 +40,22 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = 'You must be logged in!'
+      redirect_to new_session_url
+    end
+  end
+
+  def current_user?(user)
+    user && user == current_user
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url unless current_user? @user
   end
 end
