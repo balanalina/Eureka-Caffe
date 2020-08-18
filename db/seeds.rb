6bbb986a329b91a87005853789ffe7bcb230bf73
Balan Alina <alinabalan16.ab@gmail.com>
2020-08-18 17:58:43 +0300

    add cart to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,6 +25,7 @@ products_array.each do |hash|
   new_product.image.attach(io: File.open(file_path), filename: image)
 end
 
-User.create(name: 'buna siua', email: 'alina@jmek.com', password: '1111111', activation: true)
+user = User.create(name: 'buna siua', email: 'alina@jmek.com', password: '1111111', activation: true)
+Cart.create(user_id: user.id)
 
 
