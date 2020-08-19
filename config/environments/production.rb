34d6b16524d61589405e8c6d5313e348bf867444
Balan Alina <alinabalan16.ab@gmail.com>
2020-08-19 11:23:07 +0300

    try with sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,13 +68,14 @@ Rails.application.configure do
   host = 'https://shrouded-dusk-85229.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :port           => ENV['MAILGUN_SMTP_PORT'],
-      :address        => ENV['MAILGUN_SMTP_SERVER'],
-      :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-      :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-      :domain         => 'shrouded_dusk-85229.herokuapp.com',
-      :authentication => :plain
+  ActionMailer::Base.smtp_settings = {
+      user_name: ENV['SENDGRID_USERNAME'],
+      password: ENV['SENDGRID_PASSWORD'],
+      domain: 'shrouded-dusk-85229.herokuapp.com',
+      address: 'smtp.sendgrid.net',
+      port: 587,
+      authentication: :plain,
+      enable_starttls_auto: true
   }
   
 
